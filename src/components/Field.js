11875119c5f658b467721fbab8575d677c6418d4
Roland Pákai <roland.pakai@liferay.com>
2023-02-01 17:30:30 +0100

    Display values in percent

--- src/components/Field.js
+++ src/components/Field.js
@@ -51,7 +51,7 @@ const Field = ( props ) => {
       popoverContent = (
         <TableBody>
           <TableRow key={`${idd}-playerId`}><TableCell align="left">HeroId</TableCell><TableCell align="right">{playerId}</TableCell></TableRow>
-          <TableRow key={`${idd}-health`}><TableCell align="left">Health</TableCell><TableCell align="right">{health}</TableCell></TableRow>
+          <TableRow key={`${idd}-health`}><TableCell align="left">Health</TableCell><TableCell align="right">{health.toLocaleString("en", {style: "percent"})}</TableCell></TableRow>
           <TableRow key={`${idd}-score`}><TableCell align="left">Score</TableCell><TableCell align="right">{score}</TableCell></TableRow>
         </TableBody>
       );
@@ -65,11 +65,11 @@ const Field = ( props ) => {
       popoverContent = (
         <TableBody>
           <TableRow key={`${idd}-id`}><TableCell align="left">Id</TableCell><TableCell align="right">{id}</TableCell></TableRow>
-          <TableRow key={`${idd}-health`}><TableCell align="left">Health</TableCell><TableCell align="right">{health}</TableCell></TableRow>
-          <TableRow key={`${idd}-moveProbability`}><TableCell align="left">Move Probability</TableCell><TableCell align="right">{moveProbability}</TableCell></TableRow>
-          <TableRow key={`${idd}-shootProbability`}><TableCell align="left">Shoot Probability</TableCell><TableCell align="right">{shootProbability}</TableCell></TableRow>
-          <TableRow key={`${idd}-bulletDamage`}><TableCell align="left">Bullet Damage</TableCell><TableCell align="right">{bulletDamage}</TableCell></TableRow>
-          <TableRow key={`${idd}-onTouchDamage`}><TableCell align="left">Touch Damage</TableCell><TableCell align="right">{onTouchDamage}</TableCell></TableRow>
+          <TableRow key={`${idd}-health`}><TableCell align="left">Health</TableCell><TableCell align="right">{health.toLocaleString("en", {style: "percent"})}</TableCell></TableRow>
+          <TableRow key={`${idd}-moveProbability`}><TableCell align="left">Move Probability</TableCell><TableCell align="right">{moveProbability.toLocaleString("en", {style: "percent"})}</TableCell></TableRow>
+          <TableRow key={`${idd}-shootProbability`}><TableCell align="left">Shoot Probability</TableCell><TableCell align="right">{shootProbability.toLocaleString("en", {style: "percent"})}</TableCell></TableRow>
+          <TableRow key={`${idd}-bulletDamage`}><TableCell align="left">Bullet Damage</TableCell><TableCell align="right">{bulletDamage.toLocaleString("en", {style: "percent"})}</TableCell></TableRow>
+          <TableRow key={`${idd}-onTouchDamage`}><TableCell align="left">Touch Damage</TableCell><TableCell align="right">{onTouchDamage.toLocaleString("en", {style: "percent"})}</TableCell></TableRow>
         </TableBody>
       );
       break;
