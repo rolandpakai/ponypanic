545c9944338881d42f5e97d2330e4eed480aecb9
Roland Pákai <roland.pakai@liferay.com>
2023-02-12 10:31:56 +0100

    Merge TitleContainer into TitleScreen - Remove related test

--- src/components/TitleScreen.jsx
+++ src/components/TitleScreen.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 import Footer from "./Footer";
-import TitleContainer from "./TitleContainer";
+
+import { ThemeContext } from "../contexts/ThemeContext";
+import TitleContainerMainMenu from "./TitleContainerMainMenu";
 
 const TitleScreen = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <>
-      <TitleContainer />
+      <Box>
+        <Container maxWidth="md">
+          <Box
+            justifyContent="center"
+            sx={{
+              height: "550px",
+              padding: "2rem",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+              backgroundImage: `url('./themes/${theme}/title-screen.png')`,
+            }}
+          />
+        </Container>
+        <TitleContainerMainMenu />
+      </Box>
       <Footer />
     </>
   );
