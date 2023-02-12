3fb1e53b6ca403dbac9540df53ed7860b2a34455
Roland Pákai <roland.pakai@liferay.com>
2023-02-12 15:13:09 +0100

    Create existsGameState and useEffect functions

--- src/components/TitleStoryModeMenu.jsx
+++ src/components/TitleStoryModeMenu.jsx
@@ -1,21 +1,53 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
 
 import { GameStateContext } from "../contexts/GameStateContext";
-import { GAME_STATE } from "../utils/constants";
+import { GAME_STATE, LOCAL_STORAGE_STATE_NAME } from "../utils/constants";
 import Button from "./Button";
 
 const TitleContainerModeMenu = () => {
-  const { updateGameState } = useContext(GameStateContext);
+  const { gameState, updateGameState } = useContext(GameStateContext);
 
   const onClickNewGameHandler = () => {
-    updateGameState(GAME_STATE.NEW);
+    const newGameState = {
+      state: GAME_STATE.NEW,
+    };
+
+    updateGameState(newGameState);
   };
 
   const onClickContinueHandler = () => {
-    updateGameState(GAME_STATE.NEW);
+    const newGameState = {
+      ...gameState,
+      state: GAME_STATE.CONTINUE,
+    };
+    updateGameState(newGameState);
   };
+
+  const existsGameState = () => {
+    let exists = false;
+    const localStorageState = localStorage.getItem(LOCAL_STORAGE_STATE_NAME);
+
+    if (localStorageState && localStorageState !== "undefined") {
+      exists = true;
+    }
+
+    return exists;
+  };
+
+  useEffect(() => {
+    if (existsGameState()) {
+      const localStorageState = localStorage.getItem(LOCAL_STORAGE_STATE_NAME);
+      const storageState = JSON.parse(localStorageState);
+      const newGameState = {
+        ...gameState,
+        value: storageState,
+      };
+
+      updateGameState(newGameState);
+    }
+  }, []);
 
   return (
     <Container maxWidth="xs">
@@ -23,7 +55,9 @@ const TitleContainerModeMenu = () => {
         <Button onClick={onClickNewGameHandler}>NEW GAME</Button>
       </Stack>
       <Stack direction="row" justifyContent="center" p="10px" maxWidth="xs">
-        <Button onClick={onClickContinueHandler}>CONTINUE</Button>
+        <Button disabled={!existsGameState()} onClick={onClickContinueHandler}>
+          CONTINUE
+        </Button>
       </Stack>
     </Container>
   );
